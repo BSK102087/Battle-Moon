a40a839b8b576a69a28710d168857a2d33c85f22
BSK102087 <106853339+BSK102087@users.noreply.github.com>
2022-06-04 11:02:04 -0400

    Add files via upload

---
+++ script/c6033.lua
@@ -0,0 +1,72 @@
+--Beast World Thrusters
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsSetCard,0x1f4))
+	--Draw 2
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.drcon)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+    --Switch ATK & DEF
+    local e3=Effect.CreateEffect(c)	
+    e3:SetType(EFFECT_TYPE_EQUIP)
+    e3:SetCode(EFFECT_SWAP_AD)
+    c:RegisterEffect(e3)
+end
+function s.spfilter(c)
+    return c:IsSetCard(0x1f4) and c:IsType(TYPE_MONSTER) and c:IsLevelBelow(6) and c:IsAbleToRemove()
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetHandler():GetPreviousEquipTarget()
+	return e:GetHandler():IsReason(REASON_LOST_TARGET) and not ec:IsLocation(LOCATION_ONFIELD+LOCATION_OVERLAY)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+	if Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)~=0 then
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,2))
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e2:SetLabelObject(g:GetFirst())
+		e2:SetCountLimit(1)
+		e2:SetCondition(s.discon)
+		e2:SetOperation(s.spbop)
+		Duel.RegisterEffect(e2,tp)
+		end
+	end
+end
+function s.discon(e,c)
+	if e:GetLabelObject():IsLocation(LOCATION_REMOVED) then
+		return true
+	else
+		e:Reset()
+		return false
+	end
+end
+function s.spbop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
