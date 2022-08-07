096b40fcd55a17a8f36d645e3effff4f36cb0db8
BSK102087 <106853339+BSK102087@users.noreply.github.com>
2022-08-07 12:03:14 -0400

    Update c6022.lua

--- script/c6022.lua
+++ script/c6022.lua
@@ -116,6 +116,7 @@ local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	Duel.Release(g,REASON_COST)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,LOCATION_GRAVE)
 end
