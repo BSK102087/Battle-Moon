d7e7b135c9b36b26be7f335f0e18577478bc6113
BSK102087 <106853339+BSK102087@users.noreply.github.com>
2023-01-24 18:44:42 -0500

    Update c6014.lua

--- script/c6014.lua
+++ script/c6014.lua
@@ -15,15 +15,14 @@ function s.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_SET_PROC)
 	c:RegisterEffect(e2)
-	--Summon with Pendulum Cards
+	--Tribute Summon using Pendulum Cards
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e3:SetCode(EFFECT_SUMMON_PROC)
-	e3:SetCondition(s.trcon)
-	e3:SetOperation(s.trop)
-	e3:SetValue(SUMMON_TYPE_TRIBUTE)
+	e3:SetCode(EFFECT_ADD_EXTRA_TRIBUTE)
+	e3:SetTargetRange(LOCATION_SZONE,0)
+	e3:SetTarget(s.trtg)
+	e3:SetValue(POS_FACEUP)
 	c:RegisterEffect(e3)
 	--Cannot Attack this card (Diff column)
 	local e4=Effect.CreateEffect(c)	
@@ -114,85 +113,9 @@ function s.ntop(e,tp,eg,ep,ev,re,r,rp,c)
 	e4:SetValue(TYPE_EFFECT)
 	c:RegisterEffect(e4)
 end
-function s.trfilter(c)
+function s.trtg(e,c)
 	return c:IsType(TYPE_PENDULUM) and c:IsSetCard(0x1f4) and c:IsReleasable()
 end
-function s.exfilter(c,g,sc)
-	if not c:IsReleasable() or g:IsContains(c) or c:IsHasEffect(EFFECT_EXTRA_RELEASE) then return false end
-	local rele=c:GetCardEffect(EFFECT_EXTRA_RELEASE_SUM)
-	if rele then
-		local remct,ct,flag=rele:GetCountLimit()
-		if remct<=0 then return false end
-	else return false end
-	local sume={c:GetCardEffect(EFFECT_UNRELEASABLE_SUM)}
-	for _,te in ipairs(sume) do
-		if type(te:GetValue())=='function' then
-			if te:GetValue()(te,sc) then return false end
-		else return false end
-	end
-	return true
-end
-function s.val(c,sc,ma)
-	local eff3={c:GetCardEffect(EFFECT_TRIPLE_TRIBUTE)}
-	if ma>=3 then
-		for _,te in ipairs(eff3) do
-			if te:GetValue()(te,sc) then return 0x30001 end
-		end
-	end
-	local eff2={c:GetCardEffect(EFFECT_DOUBLE_TRIBUTE)}
-	for _,te in ipairs(eff2) do
-		if te:GetValue()(te,sc) then return 0x20001 end
-	end
-	return 1
-end
-function s.req(c)
-	return c:IsType(TYPE_PENDULUM) and c:IsSetCard(0x1f4) and c:IsLocation(LOCATION_PZONE)
-end
-function s.unreq(c,tp)
-	return c:IsControler(1-tp) and not c:IsHasEffect(EFFECT_EXTRA_RELEASE) and c:IsHasEffect(EFFECT_EXTRA_RELEASE_SUM)
-end
-function s.rescon(sg,e,tp,mg)
-	local c=e:GetHandler()
-	local mi,ma=c:GetTributeRequirement()
-	if mi<1 then mi=ma end
-	if not sg:IsExists(s.req,1,nil) or not aux.ChkfMMZ(1)(sg,e,tp,mg) 
-		or sg:FilterCount(s.unreq,nil,tp)>1 then return false end
-	local ct=sg:GetCount()
-	return sg:CheckWithSumEqual(s.val,mi,ct,ct,c,ma) or sg:CheckWithSumEqual(s.val,ma,ct,ct,c,ma)
-end
-function s.trcon(e,c,minc)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local g=Duel.GetTributeGroup(c)
-	local exg=Duel.GetMatchingGroup(s.otfilter,tp,LOCATION_PZONE,0,nil)
-	g:Merge(exg)
-	local opg=Duel.GetMatchingGroup(s.exfilter,tp,0,LOCATION_MZONE,nil,g,c)
-	g:Merge(opg)
-	local mi,ma=c:GetTributeRequirement()
-	if mi<minc then mi=minc end
-	if ma<mi then return false end
-	return ma>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>-ma and aux.SelectUnselectGroup(g,e,tp,1,ma,s.rescon,0)
-end
-function s.trop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetTributeGroup(c)
-	local exg=Duel.GetMatchingGroup(s.otfilter,tp,LOCATION_PZONE,0,nil)
-	g:Merge(exg)
-	local opg=Duel.GetMatchingGroup(s.exfilter,tp,0,LOCATION_MZONE,nil,g,c)
-	g:Merge(opg)
-	local mi,ma=c:GetTributeRequirement()
-	if mi<1 then mi=1 end
-	local sg=aux.SelectUnselectGroup(g,e,tp,mi,ma,s.rescon,1,tp,HINTMSG_RELEASE)
-	local remc=sg:Filter(s.unreq,nil,tp):GetFirst()
-	if remc then
-		local rele=remc:GetCardEffect(EFFECT_EXTRA_RELEASE_SUM)
-		rele:Reset()
-	end
-	c:SetMaterial(sg)
-	Duel.Release(sg,REASON_SUMMON+REASON_MATERIAL)
-end
-function s.tg(e,c)
-	return c~=e:GetHandler() and c:IsFaceup() 
-end	
 function s.colcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsType(TYPE_EFFECT)
 end	
