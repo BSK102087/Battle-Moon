5218b2682ba785cd23b5576ab1f739a58f38bd81
BSK102087 <106853339+BSK102087@users.noreply.github.com>
2023-01-24 19:09:45 -0500

    Update c6020.lua

--- script/c6020.lua
+++ script/c6020.lua
@@ -71,7 +71,7 @@ function s.bmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.bmop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.bmfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.bmfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
     if #g>0 then
         Duel.SendtoHand(g,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,g)
