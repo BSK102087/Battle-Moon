dba9db1128720aae6f675f75728d4877ed641e5b
BSK102087 <106853339+BSK102087@users.noreply.github.com>
2023-09-25 18:41:18 -0400

    Update c6041.lua

--- script/c6041.lua
+++ script/c6041.lua
@@ -122,8 +122,9 @@ end
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	if Duel.ReturnToField(tc) then
-		local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(aux.TRUE),tp,LOCATION_GRAVE,0,nil)
+		local g=Duel.GetMatchingGroup(aux.TRUE),tp,LOCATION_REMOVED,0,nil)
 		if #g==0 then return end
-		
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_REMOVED,0,1,1,nil)
 		
 end
