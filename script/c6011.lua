dc0e3c0703824152276dd36b023d0244fec2ce55
BSKDAEMON <106853339+BSK102087@users.noreply.github.com>
2023-10-02 21:35:24 -0400

    Add files via upload

---
+++ script/c6011.lua
@@ -0,0 +1,155 @@
+--Cyclone Battle Moon
+local s,id=GetID()
+function s.initial_effect(c)
+	Pendulum.AddProcedure(c)
+	--Control only 1
+	c:SetUniqueOnField(1,0,id)
+	--Normal Summon & Set with no Tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetCondition(s.ntcon)
+	e1:SetOperation(s.ntop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_PROC)
+	c:RegisterEffect(e2)
+	--Tribute Summon using Pendulum Cards
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EFFECT_ADD_EXTRA_TRIBUTE)
+	e3:SetTargetRange(LOCATION_SZONE,0)
+	e3:SetTarget(s.trtg)
+	e3:SetValue(POS_FACEUP)
+	c:RegisterEffect(e3)
+	--Change to DEF Position (Opponent only)(Non-WIND)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_SET_POSITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.poscon)
+	e4:SetTarget(s.target)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetValue(POS_FACEUP_DEFENSE)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	c:RegisterEffect(e5)
+	--Cannot Attack
+	local e6=e5:Clone()
+	e6:SetCode(EFFECT_CANNOT_ATTACK)	
+	c:RegisterEffect(e6)
+	--Special Summon
+	local e7=Effect.CreateEffect(c)
+	e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e7:SetDescription(aux.Stringid(id,2))
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e7:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e7:SetCode(EVENT_LEAVE_FIELD)
+	e7:SetCondition(s.spcon)
+	e7:SetTarget(s.sptg)
+	e7:SetOperation(s.spop)
+	c:RegisterEffect(e7)
+	--Replace
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e8:SetCode(EFFECT_DESTROY_REPLACE)
+	e8:SetRange(LOCATION_PZONE)
+	e8:SetTarget(s.reptg)
+	e8:SetValue(s.repval)
+	e8:SetOperation(s.repop)
+	c:RegisterEffect(e8)
+	--Non-WIND -ATK & DEF
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_FIELD)
+	e9:SetCode(EFFECT_UPDATE_ATTACK)
+	e9:SetRange(LOCATION_PZONE)
+	e9:SetTarget(s.target)
+	e9:SetTargetRange(0,LOCATION_MZONE)
+	e9:SetValue(-500)
+	c:RegisterEffect(e9)
+	local e10=e9:Clone()
+	e10:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e10)
+end
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and c:GetLevel()>4 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+function s.ntop(e,tp,eg,ep,ev,re,r,rp,c)
+	--change base attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetReset(RESET_EVENT+0xff0000)
+	e1:SetCode(EFFECT_SET_BASE_ATTACK)
+	e1:SetValue(1900)
+	c:RegisterEffect(e1)
+	--change base defense
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_BASE_DEFENSE)
+	e2:SetValue(1900)
+	c:RegisterEffect(e2)
+	--Normal Monster
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetReset(RESET_EVENT+0xff0000)
+	e3:SetCode(EFFECT_ADD_TYPE)
+	e3:SetValue(TYPE_NORMAL)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_REMOVE_TYPE)
+	e4:SetValue(TYPE_EFFECT)
+	c:RegisterEffect(e4)
+end
+function s.trtg(e,c)
+	return c:IsType(TYPE_PENDULUM) and c:IsSetCard(0x1f4) and c:IsReleasable()
+end
+function s.tg(e,c)
+	return c~=e:GetHandler() and c:IsFaceup()
+end	
+function s.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsType(TYPE_EFFECT)
+end
+function s.target(e,c)
+	return not c:IsAttribute(0x08) and c:IsFaceup()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_TRIBUTE) and c:IsPreviousLocation(LOCATION_MZONE)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x1f4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP) and c:IsLevelBelow(6)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end	
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and (c:IsSetCard(0x1f4) or c:IsAttribute(0x08))
+		and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT+REASON_REPLACE)
+end	
---
+++ script/c6020.lua
@@ -0,0 +1,100 @@
+--Red Moon Luna
+local s,id=GetID()
+function s.initial_effect(c)
+	--Link Summon
+	Link.AddProcedure(c,nil,2,2,s.lcheck)
+	c:EnableReviveLimit()
+	--Add to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+    e1:SetCondition(s.bmcon)
+    e1:SetTarget(s.bmtg)
+    e1:SetOperation(s.bmop)
+    c:RegisterEffect(e1)
+	--Special Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})	
+	e2:SetTarget(s.pentg)
+	e2:SetOperation(s.penop)
+	c:RegisterEffect(e2)
+end
+function s.lcheck(g,lc,tp)
+	return g:IsExists(Card.IsSetCard,1,nil,0x1f4,lc,SUMMON_TYPE_LINK,tp) 
+end
+function s.bmcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function s.bmfilter(c)
+    return c:IsSetCard(0x1f4) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.spfilter(c,e,tp,zone)
+	return c:IsSetCard(0x1f4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
+end
+function s.bmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.bmfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.bmop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.bmfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+		if Duel.GetFieldGroupCount(tp,LOCATION_MMZONE,0)>0 then return end
+		local zone=e:GetHandler():GetLinkedZone(tp)
+		local g1=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+		if #g1>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then 
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local g1=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,zone)
+			local sc=g1:GetFirst()
+			if not sc then return end
+			Duel.BreakEffect()
+			if Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP,zone) then
+				local e3=Effect.CreateEffect(c)
+				e3:SetType(EFFECT_TYPE_SINGLE)
+				e3:SetCode(EFFECT_DISABLE)
+				e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+				sc:RegisterEffect(e3)
+				local e4=Effect.CreateEffect(c)
+				e4:SetType(EFFECT_TYPE_SINGLE)
+				e4:SetCode(EFFECT_DISABLE_EFFECT)
+				e4:SetReset(RESET_EVENT+RESETS_STANDARD)
+				sc:RegisterEffect(e4)
+			end
+			Duel.SpecialSummonComplete()
+		end
+    end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1f4) and c:IsType(TYPE_PENDULUM)
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.CheckPendulumZones(tp)
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,LOCATION_GRAVE)
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not Duel.CheckPendulumZones(tp) and not c:IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then if
+		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true) then
+			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
