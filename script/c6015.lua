8d04ad440a2d8e82b1f4b841eee3ebef7bf7efd8
BSKDAEMON <106853339+BSK102087@users.noreply.github.com>
2023-11-12 23:54:11 -0500

    Update c6015.lua

--- script/c6015.lua
+++ script/c6015.lua
@@ -114,8 +114,8 @@ end
 function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local at=Duel.GetAttacker()
 	local bc=Duel.GetAttackTarget()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.ctfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,at,e,tp) or 
-		Duel.IsExistingMatchingCard(s.ctfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,bc,e,tp) end 
+	if chk==0 then return Duel.IsExistingMatchingCard(s.ctfilter,tp,0,LOCATION_MZONE,1,at,e,tp) or 
+		Duel.IsExistingMatchingCard(s.ctfilter,tp,LOCATION_MZONE,0,1,bc,e,tp) end 
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	local at=Duel.GetAttacker()
