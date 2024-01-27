8388d7057a0442d3102958b105ce3be46c651c52
BSKDAEMON <106853339+BSK102087@users.noreply.github.com>
2024-01-26 20:02:15 -0500

    Update c6006.lua

--- script/c6006.lua
+++ script/c6006.lua
@@ -9,7 +9,6 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCountLimit(1,0x190)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
@@ -76,28 +75,32 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp,chk)
 		d:RegisterEffect(e2)
 	end
 end
-function s.hsfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_NORMAL)
-end
 function s.drcfilter(c)
 	return not c:IsCode(id) and c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.hsfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_NORMAL),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-	if Duel.IsExistingMatchingCard(s.drcfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) then
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DRAW)
-	end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,nil,1,tp,1)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)	
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(id,2))
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(s.aclimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 		if Duel.IsPlayerCanDraw(tp,1) 
 			and Duel.IsExistingMatchingCard(s.drcfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 			and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
@@ -105,4 +108,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 			Duel.Draw(tp,1,REASON_EFFECT)
 		end
 	end
+end
+function s.aclimit(e,re,tp)
+	return re:GetHandler():IsType(TYPE_PENDULUM) and re:GetHandler():IsSetCard(0x190) and re:GetHandler():IsLocation(LOCATION_PZONE)
 end
