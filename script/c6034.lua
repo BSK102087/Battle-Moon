0cb5341d9e1c95920e0f2aab71d52087684ef43d
BSKDAEMON <106853339+BSK102087@users.noreply.github.com>
2024-10-22 23:48:25 -0400

    Update c6034.lua

--- script/c6034.lua
+++ script/c6034.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
-	--Special Summon
+	--Activate 1 of these effects
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
@@ -17,20 +17,8 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return eg:IsExists(s.atrfilter,1,nil,tp,rp) end)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetTarget(s.spsttg)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,{id,2})
-	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return eg:IsExists(s.atr1filter,1,nil,tp,rp) end)
-	e3:SetTarget(s.sttg)
-	e3:SetOperation(s.stop)
-	c:RegisterEffect(e3)
 end
 s.listed_card_types={TYPE_GEMINI}
 function s.filter(c)
@@ -49,15 +37,34 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.atrfilter(c,tp,rp)
-	return c:IsPreviousLocation(LOCATION_PZONE) and c:IsSetCard(0x1f4) and c:IsPreviousControler(tp)
+	return c:IsSetCard(0x1f4) and c:IsPreviousControler(tp)
 end
 function s.spfilter(c,e,tp)
 	return (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup()) and c:IsSetCard(0x1f4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+function s.stfilter(c,e,sp)
+	return (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup()) and c:IsCode(6030,6031,6032,6033,6034,6035,6036,6037,6038) and (c:IsAbleToHand() or c:IsSSetable())
+end
+function s.spsttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) and not Duel.HasFlagEffect(tp,id)
+	local b2=Duel.IsExistingMatchingCard(s.stfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) and not Duel.HasFlagEffect(tp,id+1)
+	if chk==0 then return b1 or b2 end
+	local op=0
+	if b1 and b2 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
+	elseif b1 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,1))
+	else
+		op=Duel.SelectOption(tp,aux.Stringid(id,2))+1
+	end
+	if op==0 then
+		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		e:SetOperation(s.spop)
+	else
+		Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,1)
+		e:SetOperation(s.stop)
+	end
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
@@ -67,15 +74,6 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if tc then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
-end
-function s.atr1filter(c,tp,rp)
-	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSetCard(0x1f4) and c:IsPreviousControler(tp)
-end
-function s.stfilter(c,e,sp)
-	return (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup()) and c:IsCode(6030,6031,6032,6033,6034,6035,6036,6037,6038) and (c:IsAbleToHand() or c:IsSSetable())
-end
-function s.sttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.stfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
 end
 function s.stop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
